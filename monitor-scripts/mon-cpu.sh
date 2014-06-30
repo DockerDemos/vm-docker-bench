e12f47989d32facea95812e8bb1bfa649a79c861
Chris Collins <collins.christopher@gmail.com>
2014-06-30 10:21:30 -0400

    modified for output to ssh

--- monitor-scripts/mon-cpu.sh
+++ monitor-scripts/mon-cpu.sh
@@ -11,21 +11,12 @@
 #   steal: involuntary wait
 HOSTNAME="$(hostname)"
 DATE="$(date '+%Y%m%d')"
-BASE='/bench/results'
-LOGFILE="$BASE/$DATE-$HOSTNAME-cpu.csv"
 
-if [[ ! -d $BASE ]] ; then
-  sudo mkdir -p $BASE
-fi
-
-sudo touch $LOGFILE
-sudo chmod 666 $LOGFILE
-
-echo 'x,user,nice,system,idle,iowait,irq,softirq,steal,guest,guest_nice' >> $LOGFILE
+echo 'x,user,nice,system,idle,iowait,irq,softirq,steal,guest,guest_nice' 
 
 
 while true ; do 
   echo "$(cat /proc/stat |head -n 1\
-  |sed -e 's/^[ \t]*//' | sed -e 's/[ ][ ]*/,/g')" >> $LOGFILE
+  |sed -e 's/^[ \t]*//' | sed -e 's/[ ][ ]*/,/g')"
   sleep 1s
 done
