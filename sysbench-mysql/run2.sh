f4d8e892cc2908658a3030dab28972794eb0312e
Chris Collins <collins.christopher@gmail.com>
2014-07-10 14:30:05 -0400

    formatting and cleanup

--- sysbench-mysql/run2.sh
+++ sysbench-mysql/run2.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+USER='admin'
+PASS='rootmysqlpassword'
 
 /run.sh &
 
 sleep 15
 
-sysbench --test=oltp --oltp-table-size=1000000 --mysql-db=test --mysql-user=admin --mysql-password=rootmysqlpassword prepare
-sysbench --test=oltp --oltp-table-size=1000000 --mysql-db=test --mysql-user=admin --mysql-password=rootmysqlpassword --max-time=60 --oltp-read-only=on --max-requests=0 --num-threads=8 run
+sysbench --test=oltp --oltp-table-size=1000000 --mysql-db=test \
+	 --mysql-user=$USER --mysql-password=$PASS prepare
 
+sysbench --test=oltp --oltp-table-size=1000000 --mysql-db=test \
+	 --mysql-user=$USER --mysql-password=$PASS --max-time=60 \
+	 --oltp-read-only=on --max-requests=0 --num-threads=8 run
+
+sysbench --test=oltp --mysql-db=test --mysql-user=$USER \
+	 --mysql-password=$PASS cleanup
